aa68d8da79c1b49a0bb0fb463732768d4a7ef626
Devin <devin@xuncetech.com>
2022-04-20 14:53:33 +0800

    ca

--- jfm-web/src/pages/header/downloadApp/downloadApp.vue
+++ jfm-web/src/pages/header/downloadApp/downloadApp.vue
@@ -1,25 +1,28 @@
 <template>
-    <div style="margin-top: 50px;box-shadow: inset 0px 4px 4px 0px rgb(242 242 242);">
-        <div style="display:flex">
-            <div></div>
-            <div></div>
-        </div>
-        <h1 class="animate__animated animate__fadeIn" v-if="show">An animated element</h1>
-        <el-button @click="show = !show">Button</el-button>
+  <div
+    style="margin-top: 50px; box-shadow: inset 0px 4px 4px 0px rgb(242 242 242)"
+  >
+    <div style="display: flex">
+      <div></div>
+      <div></div>
     </div>
+    <h1 class="animate__animated animate__fadeIn" v-if="show">
+      An animated element
+    </h1>
+    <el-button @click="show = !show">Button</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"DownloadApp",
-    data(){
-        return {
-            show:true
-        }
-    }
-}
+  name: "DownloadApp",
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
