835b60f58385b92a2c62d63a904f3deeef8150d6
linshihao <lsh182@sina.com>
2022-04-20 23:00:30 +0800

    2

--- jfm-web/src/pages/header/downloadApp/downloadApp.vue
+++ jfm-web/src/pages/header/downloadApp/downloadApp.vue
@@ -1,14 +1,12 @@
 <template>
-  <div
-    style="margin-top: 50px; box-shadow: inset 0px 4px 4px 0px rgb(242 242 242)"
-  >
-    <div style="display: flex">
-      <div></div>
-      <div></div>
+  <div style="margin-top: 50px; box-shadow: inset 0px 4px 4px 0px rgb(242 242 242)">
+    <div style="display: flex;" class="bg-img">
+      <div style="width:50%">
+        <img />
+      </div>
+      <div style="width:50%"></div>
     </div>
-    <h1 class="animate__animated animate__fadeIn" v-if="show">
-      An animated element
-    </h1>
+    <h1 class="animate__animated animate__fadeIn" v-if="show">An animated element</h1>
     <el-button @click="show = !show">Button</el-button>
   </div>
 </template>
@@ -18,11 +16,18 @@ export default {
   name: "DownloadApp",
   data() {
     return {
-      show: true,
+      show: true
     };
-  },
+  }
 };
 </script>
 
 <style scoped>
+.bg-img {
+  position: relative;
+  background: #fff url("~@/pages/header/downloadApp/resources/bg1.png")
+    no-repeat;
+  margin: 0 auto;
+  height: 689px;
+}
 </style>
