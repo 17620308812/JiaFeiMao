b822059f064bdbd87a5cd6e77190ad14f30308a6
Devin <devin@xuncetech.com>
2022-04-26 14:19:06 +0800

    33

--- jfm-web/src/pages/header/game/game.vue
+++ jfm-web/src/pages/header/game/game.vue
@@ -1,13 +1,16 @@
 <template>
-    <div style="margin-top: 50px;box-shadow: inset 0px 4px 4px 0px rgb(242 242 242);">123</div>
+  <div
+    style="margin-top: 50px; box-shadow: inset 0px 4px 4px 0px rgb(242 242 242)"
+  >
+    <div style="width: 60%; display: flex; margin: 0px auto">123</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"Game"
-}
+  name: "Game",
+};
 </script>
 
 <style scoped>
-
 </style>
